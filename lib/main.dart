61f68491e7dc13afd11f07f2bead6cee583f8a0b
Anthony Arinze <anthonyarinze28@gmail.com>
2023-05-17 19:17:27 +0100

    Nav Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/pages/home_page.dart';
+import 'package:spotify_clone/pages/search_page.dart';
+import 'package:spotify_clone/pages/your_library.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int index = 0;
+
+  final screens = [const Home(), const SearchPage(), const YourLibrary()];
+
+  final items = <BottomNavigationBarItem>[
+    const BottomNavigationBarItem(
+      label: 'Home',
+      icon: Icon(Icons.home_filled),
+      backgroundColor: Colors.yellow,
+    ),
+    const BottomNavigationBarItem(
+      label: 'Search',
+      icon: Icon(Icons.search),
+    ),
+    const BottomNavigationBarItem(
+      label: 'Your Library',
+      icon: Icon(Icons.library_music),
+    ),
+  ];
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Spotify Clone',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
@@ -23,7 +50,17 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const Home(),
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        bottomNavigationBar: BottomNavigationBar(
+          items: items,
+          currentIndex: index,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          onTap: (value) => setState(() => index = value),
+        ),
+        body: screens[index],
+      ),
     );
   }
 }
